be325c3e36e79badfb7e5f5b9e7251a9809834a4
veggielane <chris@lane.org.uk>
2021-11-17 17:37:46 +0000

    added Wrapper

---
+++ src/EngrCAD.OCWrapper/EngrCAD.OCWrapper.cpp
@@ -0,0 +1,4 @@
+#include "pch.h"
+
+#include "EngrCAD.OCWrapper.h"
+
---
+++ src/EngrCAD.OCWrapper/EngrCAD.OCWrapper.h
@@ -0,0 +1,10 @@
+#pragma once
+
+using namespace System;
+
+namespace EngrCADOCWrapper {
+	public ref class Class1
+	{
+		
+	};
+}
