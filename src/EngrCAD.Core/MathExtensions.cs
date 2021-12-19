c447f1119a5a9d8282e6ecfcd789ca563b2774a6
veggielane <chris@lane.org.uk>
2021-12-19 11:34:37 +0000

    .

---
+++ src/EngrCAD.Core/MathExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace EngrCAD.Core;
+
+public static class MathExtensions
+{
+
+    public static Vector3 Cross(this Vector3 v1, Vector3 v2) => Vector3.Cross(v1, v2);
+    public static float Dot(this Vector3 v1, Vector3 v2) => Vector3.Dot(v1, v2);
+}
--- src/EngrCAD.Core/MathHelper.cs
+++ src/EngrCAD.Core/MathHelper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -53,12 +52,5 @@ namespace EngrCAD.Core
         {
             return (float)((Math.PI / 180) * degrees);
         }
-    }
-
-    public static class MathExtensions
-    {
-
-        public static Vector3 Cross(this Vector3 v1, Vector3 v2) => Vector3.Cross(v1, v2);
-        public static float Dot(this Vector3 v1, Vector3 v2) => Vector3.Dot(v1, v2);
     }
 }
