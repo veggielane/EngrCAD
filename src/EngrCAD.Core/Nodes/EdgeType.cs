78f2fe17bdd9a78214c672e931eab7fdb5957c15
veggielane <chris@lane.org.uk>
2021-12-27 18:35:06 +0000

    Create EdgeType.cs

---
+++ src/EngrCAD.Core/Nodes/EdgeType.cs
@@ -0,0 +1,13 @@
+﻿namespace EngrCAD.Core.Nodes;
+
+public enum EdgeType
+{
+    GeomAbs_Line,
+    GeomAbs_Circle,
+    GeomAbs_Ellipse,
+    GeomAbs_Hyperbola,
+    GeomAbs_Parabola,
+    GeomAbs_BezierCurve,
+    GeomAbs_BSplineCurve,
+    GeomAbs_OffsetCurve,
+    GeomAbs_OtherCurve, }
